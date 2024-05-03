5e9bd703e393ea2a11d09ffa898233fb446a1b9f
daaaan12 <danilooc1516@gmail.com>
2024-05-03 11:40:01 -0300

    Adicionado um icone para donwload do curriculum

--- src/components/DescriptionSection.js
+++ src/components/DescriptionSection.js
@@ -1,18 +1,19 @@
 // DescriptionSection.js
-import React from 'react';
-import { useIntl } from 'react-intl';
-import { AiFillGithub } from "react-icons/ai";
-import { AiFillLinkedin } from "react-icons/ai";
-import DaniloBoneco from '../img/DaniloBoneco.png';
-import '../scss/descriptionsection.scss';
+import React from 'react'
+import { useIntl } from 'react-intl'
+import { AiFillGithub } from 'react-icons/ai'
+import { AiFillLinkedin } from 'react-icons/ai'
+import DaniloBoneco from '../img/DaniloBoneco.png'
+import '../scss/descriptionsection.scss'
+import { AiOutlineFilePdf } from 'react-icons/ai'
 
-function DescriptionSection() {
-  const intl = useIntl();
+function DescriptionSection () {
+  const intl = useIntl()
 
   return (
-    <section className="secao-descricao">
-      <div className="container-descricao">
-        <div className="">
+    <section className='secao-descricao'>
+      <div className='container-descricao'>
+        <div className=''>
           <p>{intl.formatMessage({ id: 'welcome' })}</p>
         </div>
         <h2 style={{ fontSize: '1.5rem' }}>
@@ -21,18 +22,29 @@ function DescriptionSection() {
         <p style={{ fontSize: '1.1rem' }}>
           {intl.formatMessage({ id: 'skills' })}
         </p>
-        <a href="https://github.com/daaaan12">
-          <i className="icones-sociais "><AiFillGithub /></i>
-        </a>
-        <a href="https://www.linkedin.com/in/danilo-costa-de-oliveira-66413a22b/">
-          <i className="icones-sociais"><AiFillLinkedin /></i>
-        </a>
+        <div className='container-sociais'>
+          <a href='https://github.com/daaaan12'>
+            <i className='icones-sociais '>
+              <AiFillGithub />
+            </i>
+          </a>
+          <a href='https://www.linkedin.com/in/danilo-costa-de-oliveira-66413a22b/'>
+            <i className='icones-sociais'>
+              <AiFillLinkedin />
+            </i>
+          </a>
+          <a href='/curriculoDanilo.pdf' download='curriculoDanilo.pdf'>
+            <i className='icones-sociais' alt='Icone Curriculum'>
+              <AiOutlineFilePdf />
+            </i>
+          </a>
+        </div>
       </div>
       <div>
-        <img src={DaniloBoneco} alt="imagem-boneco" width="300px" />
+        <img src={DaniloBoneco} alt='imagem-boneco' width='300px' />
       </div>
     </section>
-  );
+  )
 }
 
-export default DescriptionSection;
+export default DescriptionSection
