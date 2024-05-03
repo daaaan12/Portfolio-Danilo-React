8ff14480a510b033706d0a3f0ff2e888feaddecb
daaaan12 <danilooc1516@gmail.com>
2024-05-03 06:40:51 -0300

    Atualizações para a linguagem do Japão

--- src/App.js
+++ src/App.js
@@ -7,29 +7,56 @@ import LanguageSelector from './components/LanguageSelector';
 import Footer from './components/Footer';
 import messages_en from './translations/messages_en.json';
 import messages_pt from './translations/messages_pt.json';
+import messages_es from './translations/messages_es.json';
+import messages_fr from './translations/messages_fr.json';
+import messages_ru from './translations/messages_ru.json';
+import messages_jp from './translations/messages_jp.json';
 
 import './scss/global.scss';
 
 function App() {
   const [locale, setLocale] = useState('pt');
-  const messages = locale === 'pt' ? messages_pt : messages_en;
-  
+  let messages;
+
+  switch (locale) {
+    case 'en':
+      messages = messages_en;
+      break;
+    case 'es':
+      messages = messages_es;
+      break;
+    case 'fr':
+      messages = messages_fr;
+      break;
+    case 'ru':
+      messages = messages_ru;
+      break;
+    case 'jp':
+      messages = messages_jp;
+      break;
+    default: // Definindo pt-BR como padrão
+      messages = messages_pt;
+      break;
+  }
+
   return (
-    <IntlProvider locale={locale} messages={messages}>
-      <body>
-        <main>
-          <LanguageSelector setLocale={setLocale} />
-          <header>
-            <Header />
-          </header>
-          <section>
-            <DescriptionSection />
-          </section>
-          <ProjectsSection />
-          <Footer />
-        </main>
-      </body>
-    </IntlProvider>
+    <body>
+      <main>
+        <IntlProvider locale={locale} messages={messages}>
+          <div>
+            <LanguageSelector setLocale={setLocale} />
+            <header>
+              <Header />
+            </header>
+            <section>
+              <DescriptionSection />
+            </section>
+            <ProjectsSection />
+            <Footer />
+          </div>
+        </IntlProvider>
+      </main>
+    </body>
   );
 }
 
--- src/components/LanguageSelector.js
+++ src/components/LanguageSelector.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import bandeiraBrasil from '../img/bandeiraBrasil.png';
 import bandeiraEua from '../img/bandeiraEua.png';
+import bandeiraEspanha from '../img/bandeiraEspanha.png';
+import bandeiraFranca from '../img/bandeiraFranca.png';
+import bandeiraRussia from '../img/bandeiraRussia.png';
+import bandeiraJapao from '../img/bandeiraJapao.png';
 
 import '../scss/languageselector.scss';
 
@@ -12,6 +16,18 @@ function LanguageSelector({ setLocale }) {
       </button>
       <button onClick={() => setLocale('en')}>
         <img src={bandeiraEua} alt="Bandeira dos EUA" width="40px" />
+      </button>
+      <button onClick={() => setLocale('es')}>
+        <img src={bandeiraEspanha} alt="Bandeira da Espanha" width="40px" />
+      </button>
+      <button onClick={() => setLocale('fr')}>
+        <img src={bandeiraFranca} alt="Bandeira da França" width="40px" />
+      </button>
+      <button onClick={() => setLocale('ru')}>
+        <img src={bandeiraRussia} alt="Bandeira da Russia" width="40px" />
+      </button>
+      <button onClick={() => setLocale('jp')}>
+        <img src={bandeiraJapao} alt="Bandeira do Japão" width="40px" />
       </button>
     </div>
   );
